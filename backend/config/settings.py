3a65663a77bedb1e0f6a2605a5da4bb8962c33a5
shavadkp <shavadpravil@gmail.com>
2026-06-07 14:29:27 +0530

    Update allowed hosts for deployment

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = 'django-insecure-1=d1&l8td*xihu6pps&$=+_nr-r_x-%e#t)!vbpj5ehp9rlj#4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
